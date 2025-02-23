11bb16be76fe3807ac33a3a2f2869aef645e3c08
yangyong <yangyongauto@126.com>
2025-02-23 22:14:44 +0800

    add web automation learn

---
+++ web/s1.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+# 创建 WebDriver 对象，指明使用chrome浏览器驱动
+wd = webdriver.Chrome(service=Service(r'C:\Users\15516\Desktop\python_learn_notes\web\chromedriver-win64\chromedriver.exe'))
+
+# 调用WebDriver 对象的get方法 可以让浏览器打开指定网址
+wd.get('https://www.baidu.com')
+
+element = wd.find_element(By.ID, 'kw')
+
+element.send_keys('白月黑羽')
+
+# 程序运行完会自动关闭浏览器，就是很多人说的闪退
+# 这里加入等待用户输入，防止闪退
+input('等待回车键结束程序')
---
+++ web/s3.py
@@ -0,0 +1,6 @@
+import random
+
+print(random.random())
+
+sets = {1,2,3,4,5}
+print(sets[2])
